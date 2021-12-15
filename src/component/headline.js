950c8de07c97303d55c7f1ff7b009b347a404bfc
Lior-Yosef <lioryosef06@gmail.com>
2021-12-15 14:21:08 +0200

    first commit

---
+++ src/component/headline.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./headline.css";
+
+class Headline extends React.Component{
+    render(){
+        return(
+            <div className="container">
+            <h1 className="neonText">
+            XO Game : enter X to start
+            </h1>
+        
+         </div>)
+          
+    }
+};
+
+export default Headline;
